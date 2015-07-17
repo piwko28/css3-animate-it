03c1d8f8545a9af2fde5c0c46ce62c2fbab0a5c9
Szymon Piwowarczyk <szymon.piwowarczyk@sienn.pl>
2015-07-17 11:22:26 +0200

    Automatically added animation classes to all elements match to defined selectors

--- js/main.js
+++ js/main.js
@@ -2,8 +2,36 @@ $(document).ready(function() {
 
   var parents = ".animatedParent";
 
+  /* AUTO ADDED ANIMATIONS -- OPTIONAL -- START */
+  var selectors = {
+    'h1' : 'fadeInDown',
+    'h2' : {
+      animation : 'fadeInRight',
+      delay : 750
+    }
+  };
+  var selector, elements;
+  function updateAnimatedElements() {
+    for(selector in selectors) {
+      elements = $(selector, parents);
+      elements.addClass("animated");
+      if(typeof selectors[selector] === 'string') {
+        elements.addClass(selectors[selector]);
+      } else if(typeof selectors[selector] === 'object') {
+        if(selectors[selector].hasOwnProperty('animation')) {
+          elements.addClass(selectors[selector].animation);
+        }
+        if(selectors[selector].hasOwnProperty('delay')) {
+          elements.addClass('delay-' + selectors[selector].delay);
+        }
+      }
+    }
+  }
+  updateAnimatedElements();
+  /* AUTO ADDED ANIMATIONS -- OPTIONAL -- END */
+
   $(parents).appear();
-  
+
   $(document.body).on('appear', parents, function(e, $affected){
     $(this).find('.animated').addClass('go');
   });
